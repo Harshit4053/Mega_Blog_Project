3673b569c4ed20fdafffb1b8bd572a494dc44e5a
Harshit4053 <harshitgajipara204@gmail.com>
2025-03-13 11:39:24 +1000

    add pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,21 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "../pages/Login ";
+import AddPost from "../pages/AddPost";
+import PostForm from "./post-form/PostForm" 
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    AddPost,
+    PostForm
 }
